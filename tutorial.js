e0747ef080a639f080085453818b437cb47baef6
feliperiosvaldes <frios1@miuandes.cl>
2021-05-02 23:20:25 -0300

    Fibonacci
    funcionando

--- tutorial.js
+++ tutorial.js
@@ -1,30 +1,35 @@
 var readlineSync = require('readline-sync');
-var x = readlineSync.question("Choose a number ");
+var number = readlineSync.question("Choose a number ");
 
 function Fibonacci(n){
     a = 0
     plus = 0
     b = 1
     c = 0 
-    if (n === 0){
+    if (n === 1 || n == 0){
         console.log(0);
     }
-    else if (n == 1){
+    else if (n == 2){
         console.log(1);
     }
     else {
         while (c < n-2){
             plus = a + b  
             a = plus
-            console.log(a)
+            //console.log(a);
             b = plus + b  
-            c ++
-            console.log(b)
+            //console.log(b);
+            c +=2
             }
-        //console.log(a);
+        if (number % 2 === 0){
+            console.log(b);
+        }
+        else{
+            console.log(a);
+        }
     }
     }
-Fibonacci(x)
+Fibonacci(number)
 
 
 
